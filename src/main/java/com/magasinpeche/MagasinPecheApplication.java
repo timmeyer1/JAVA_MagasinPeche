51030acedd34cd261cf03c9a02b8cbf48ad8e726
timmeyer1 <timvameyer@gmail.com>
2024-10-23 16:38:42 +0200

    git init

---
+++ src/main/java/com/magasinpeche/MagasinPecheApplication.java
@@ -0,0 +1,13 @@
+package com.magasinpeche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagasinPecheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagasinPecheApplication.class, args);
+	}
+
+}
